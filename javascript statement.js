13ef17187dd729a45a72601c0d841c0991fa9167
shubhanshu garg <shubhanshugarg5470@gmail.com>
2023-12-13 16:47:03 -0800

    something new

---
+++ javascript statement.js
@@ -0,0 +1,21 @@
+/*when we write code or do programming according to the synatax(is set  of rules
+ that tell us how the code is comstructed amd how to write exicutable code ) of 
+ the language so we basiclly give  list of   instraction   to computer  that is 
+ exicuted by computer . so we call this instruction as statement .  
+
+javascript statement are composed of 
+keyword , value , expression , operators and comments
+
+expression ====>
+an expression is the combination of keywords ,value and operators . which computes
+according to the instructions.
+
+keywords ======>
+keyword is  reseverd word  of the programming language . its pre -defined for deffirent 
+keyword that which keyword performed which kind of task . so when we want to do perticuler 
+computer exicussion so we use keyword respectively .
+ 
+ 
+
+value =========>
+value is always a any kind of datatype 
---
+++ opraters in javascript.js
@@ -0,0 +1,107 @@
+/* as we all know when write code or when we do proggraming so basicaly we store data on memory and do 
+computation on it .
+so for computation on different type of data we used oprators . there are many oprator in javascript 
+
+1. arithmetic operator 
+2. assignment operator 
+3. comparison opertator 
+4. logical operator 
+5. bitwise operator
+6. type operator
+7. ternary operator 
+
+
+Arithmatic operator =================================>
+thse are arithmatic operator
+(+) for addition*/
+// let rampaisa = 45;
+// let seetapaisa = 50;
+// let total = (rampaisa+seetapaisa);
+// console.log(total); 
+// let pati = "ram";
+// let patni = "seeta";
+// console.log(pati+" " +patni);
+// let ram = "ravan";
+// let k = 34;
+// console.log(4+"7");
+
+// (-) for substraction 
+// let a = 67;
+// let b = 89;
+// let c =(a-b);
+// console.log(c);
+// let a = "56";
+// let b = "45";
+// let c =(a-b);
+// console.log(c);
+// let a = "ram";
+// let b = "seta";
+// let c =(a-b);
+// console.log(c);
+// it give NAN
+// let l = "he is ";
+// let n = "very ";
+// let s = "good";
+// let all = (l+n+s);
+// let half = (all - n);
+// its give NAN 
+// console.log(all);
+// console.log(half);
+
+// (*)for multification
+// let a = 67;
+// let b = 89;
+// let c =(a*b);
+// console.log(c);
+// let a = "67";
+// let b = "65";
+// let c =(a*b);
+// console.log(c);
+// let a = "ram";
+// let b = 56;
+// let c =(a*b);
+// console.log(c);
+
+// (/) for division 
+// let a = 22;
+// let b = 7;
+// let c =(a/b);
+// console.log(c);
+// let a = "67";
+// let b = "5";
+// let c =(a/b);
+// console.log(c);
+
+// (%)its return a division reminder
+// let a = 22;
+// let b = 7;
+// let c =(a%b);
+// console.log(c);
+
+// (++)incrimenting operator and (--)decrisement operator
+// let a = 5;
+// let b = a++;
+// let c = a;
+// console.log(c);
+// let a = 8;
+// let b = ++a;
+// console.log(b); 
+// so ++a and a++ is exicute differntly
+// same as (--)decrimenting operator
+
+// (**) exponational operator 
+// let a = 5;
+// let b = 2;
+// console.log(a**b);
+
+
+// assignment operators=====================================>
+// (=)to assign a value 
+// let a = 8;
+//  a += 7;
+//  its same as a =a+7
+// console.log(a);
+// its same for - , * , / ,** , % 
+
+// comparasion operator========================================>
+
